25c1e8ea172ae64208b23e46da5039ed8f40aa91
palaceparis <palaceparisg@outlook.com>
2023-01-09 16:16:23 +0800

    initial commit

---
+++ hardhat.config.js
@@ -0,0 +1,57 @@
+require("@nomicfoundation/hardhat-toolbox")
+require("dotenv").config()
+require("@nomiclabs/hardhat-etherscan")
+require("./tasks/block-number")
+require("hardhat-gas-reporter")
+require("solidity-coverage")
+require("hardhat-deploy")
+// You need to export an object to set up your config
+// Go to https://hardhat.org/config/ to learn more
+/**
+ * @type import('hardhat/config').HardhatUserConfig
+ */
+
+const COINMARKETCAP_API_KEY = process.env.COINMARKETCAP_API_KEY || ""
+const GOERLI_RPC_URL =
+    process.env.GOERLI_RPC_URL ||
+    "https://eth-goerli.alchemyapi.io/v2/your-api-key"
+const PRIVATE_KEY =
+    process.env.PRIVATE_KEY ||
+    "0x11ee3108a03081fe260ecdc106554d09d9d1209bcafd46942b10e02943effc4a"
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY || ""
+
+module.exports = {
+    defaultNetwork: "hardhat",
+    networks: {
+        hardhat: {},
+        goerli: {
+            url: GOERLI_RPC_URL,
+            accounts: [PRIVATE_KEY],
+            chainId: 5,
+            blockConfirmations: 6,
+        },
+        localhost: {
+            url: "http://localhost:8545",
+            chainId: 31337,
+        },
+    },
+    solidity: {
+        compilers: [{ version: "0.8.8" }, { version: "0.6.6" }],
+    },
+    etherscan: {
+        apiKey: ETHERSCAN_API_KEY,
+    },
+    gasReporter: {
+        enabled: false,
+        currency: "USD",
+        outputFile: "gas-report.txt",
+        noColors: true,
+        // coinmarketcap: COINMARKETCAP_API_KEY,
+    },
+    namedAccounts: {
+        deployer: {
+            default: 0,
+        },
+        user: { default: 1 },
+    },
+}
